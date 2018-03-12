d946ee8ea13c5f93aec22826c7e7b5046dea19c8
USER-20160304GT\Administrator <yitichen22@gmail.com>
2018-03-12 17:50:19 +0800

    增加账号切换包

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,5 +31,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if (app()->isLocal()) {
+            $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
+        }
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,11 @@
 
     @include('layouts._footer')
 </div>
+
+@if (app()->isLocal())
+    @include('sudosu::user-selector')
+@endif
+
 @yield('js')
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
